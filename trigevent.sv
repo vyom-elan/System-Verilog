ce9c700df073a7c4b1a59ae49d53ae7ef35deabd
Vyom <97853828+vyom-elan@users.noreply.github.com>
2023-06-28 16:36:07 +0530

    trigevent.sv

--- trigevent.sv
+++ trigevent.sv
@@ -3,13 +3,16 @@ module main;
   initial
     repeat(4) begin
       #20;
-      ->e; $display(" e triggered at %t", $time);
+      ->e; 
+      $display(" e triggered at %t", $time);
     end
   initial
     #1000 $finish;
   always begin
-    #10; if(e.triggered)
+    #10; 
+    if(e.triggered)
       $display(" e is True at %t", $time);
-    else $display(" e False %t", $time);
+    else 
+      $display(" e False %t", $time);
   end
 endmodule
